61f731088b9b1d9edafc453b31c7ea5c1e5b18de
Artem Dudkin <artem.dudkin@visitech.ru>
2022-09-18 17:29:43 +0300

    fix issues/10 Implement logrotate

--- cmd_create.js
+++ cmd_create.js
@@ -23,10 +23,11 @@ async function create(fn, opt) {
   if (res.indexOf(`${opt.name}.service`) !== -1) {
     throw new Error(`Service ${opt.name} already exists`);
   } else {
-    if ((fn.endsWith('.js') || fn.endsWith('.js')) && !fs.existsSync(fn)) {
+    if ((fn.endsWith('.js') || fn.endsWith('.sh')) && !fs.existsSync(fn)) {
       throw new Error(`File '${fn}' does not exists`);
     }
 
+    //create folders if needed
     if (currentUser !== 'root') await runScript('mkdir -p  ~/.config/systemd/user/');
     if (currentUser !== 'root') await runScript('mkdir -p  ~/log/');
     if (currentUser !== 'root') await runScript('mkdir -p  ~/bin/');
@@ -35,6 +36,7 @@ async function create(fn, opt) {
     let logFolder = getLogFolder(currentUser);
     let serviceFolder = getServiceFolder(currentUser);
 
+    //create script to start process ('#!/usr/bin/env node' for node.j spplications and '#!/bin/bash' for others)
     if (fn.endsWith('.js')) {
       fs.writeFileSync(`${scriptFolder}${opt.name}.sh`, `#!/usr/bin/env node\n\nrequire(\'${resolve(fn)}\');\n`, {mode:0o755, flag:'w'})
     } else if (fn.endsWith('.sh')) {
@@ -44,6 +46,7 @@ async function create(fn, opt) {
     }
     if (opt.user!=='root') await runScript(`chown ${opt.user} ${scriptFolder}${opt.name}.sh`);
 
+    //create script to start process from service (which uses previous script and adds paths and timestamp if needed)
     fs.writeFileSync(`${scriptFolder}${opt.name}.service.sh`, `#!/bin/bash
 
 ${fn.endsWith('.js') ? 
@@ -59,9 +62,11 @@ ${scriptFolder}${opt.name}.sh ${opt.time? `2>&1 | while IFS= read -r line; do pr
     `, {mode:0o755})
     if (opt.user!=='root') await runScript(`chown ${opt.user} ${scriptFolder}${opt.name}.service.sh`);
 
+    //create folder for logs if needed
     if (!fs.existsSync(`${logFolder}${opt.name}`)) await runScript(`mkdir ${logFolder}${opt.name}`);
     if (opt.user!=='root') await runScript(`chown ${opt.user} ${logFolder}${opt.name}`);
 
+    //create script to start service (which uses previous script)
     fs.writeFileSync(`${serviceFolder}${opt.name}.service`, `
 [Unit]
 Description=PM2SD ${opt.description || opt.name}
@@ -79,9 +84,40 @@ ExecStart=/bin/bash ${scriptFolder}${opt.name}.service.sh
 WantedBy=${currentUser==='root'?'multi-user.target':'default.target'}
     `, {mode:0o644})
 
+
+    //start and enable service
     await runScript(`systemctl ${currentUser==='root'?'':'--user'} daemon-reload`)
     await runScript(`systemctl ${currentUser==='root'?'':'--user'} start ${opt.name}`)
     await runScript(`systemctl ${currentUser==='root'?'':'--user'} enable ${opt.name}`)
+
+    //add logrotate
+    let config = `{
+      daily
+      rotate 15
+      compress
+      minsize 1M
+      noolddir
+      dateext
+
+      missingok
+      copytruncate
+      notifempty
+}\n`;
+    if (currentUser==='root') {
+      if (!fs.existsSync('/etc/logrotate.d')) {
+        console.log(clc.yellowBright('\nCannot find logrotate config. Please install logrotate.\n'));
+      } else {
+        if (!fs.existsSync('/etc/logrotate.d/pm2sd')) {
+          fs.writeFileSync('/etc/logrotate.d/pm2sd', `/var/log/pm2sd-*/*.log\n${config}`);
+        }
+      }
+    } else {
+        if (!fs.existsSync(`${os.homedir()}/.pm2sd-logrotate.conf`)) {
+          fs.writeFileSync(`${os.homedir()}/.pm2sd-logrotate.conf`, `~/log/pm2sd-*/*.log\n${config}`);
+          fs.writeFileSync(`${os.homedir()}/.pm2sd-logrotate-state`, '');
+          await runScript('(crontab -l; echo "1 * * * * /sbin/logrotate -s ~/.pm2sd-logrotate-state ~/.pm2sd-logrotate.conf")|awk \'!x[$0]++\'|crontab -');
+        }
+    }
   }
 }
 
